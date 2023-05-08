0b42ceff57c86fb08f2170be33ce6f781a1f3de6
wawanpi <muhaguskurniawan@gmail.com>
2023-05-08 15:00:03 +0700

    membuat variable

--- 202_pointer/202_pointer.cpp
+++ 202_pointer/202_pointer.cpp
@@ -12,3 +12,10 @@ void mahasiswa::showNim() {		//inplementasi method diluar class
 	cout << "No induk = " << nim << endl;
 
 }
+int main()
+{
+	mahasiswa mhs{ 1 };
+	mhs.showNim();
+
+	mahasiswa ref = mhs;
+}
